b47b787abb40a557ab5056410382b7f18cc042c6
Aisayev Duman <duman200354@mail.ru>
2022-05-20 21:52:48 +0600

    base functionality

--- script.js
+++ script.js
@@ -1,16 +1,106 @@
 'use strict';
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={615817b80933af5c558aa4fcd4a5378f}'
+const url = 'http://api.weatherstack.com/current?access_key=707f05c875594327b5cd978294e0964a';
+const node = document.querySelector('.block-result');
+const searchInput = document.querySelector('.search__input');
+const searchBtn = document.querySelector('.search__button');
+const searchForm = document.querySelector('.wrap-search');
+const form = document.querySelector('.form');
 
-const api_url = "https://randomuser.me/api/";
 
-async function getUser() {
-    const response = await fetch(api_url);
+let database = {
+    city: 'London',
+    windSpeed: 0,
+    feelslike: 0,
+    humidity: 0,
+    temperature: 0,
+    country: 'United Kingdom'
+};
+
+async function fetchData() {
+    const response = await fetch(`${url}&query=${database.city}`);
     // Parsing it to JSON format
     const data = await response.json();
-    const info = data.results;
-    console.log(info);
-    console.log(info['nat']);
-}
 
-getUser();
+    const { 
+        current: { feelslike, wind_speed: windSpeed, humidity, temperature, weather_descriptions: description },
+        location: { country },
+    } = data;
+
+    database = {
+        ...database,
+        windSpeed,
+        feelslike,
+        humidity,
+        description,
+        temperature,
+        country,
+    };
+
+    console.log(data);
+
+    renderComponent();
+};
+
+const markup = () => {
+
+    const { city, windSpeed, feelslike, humidity, temperature, country, description } = database;
+
+    return `
+    <div class="result-header">
+        <div class="header__title">Weather</div>
+        <img src="img/search.svg" width="24px" height="24px" alt="search">
+    </div>
+    <div class="degree-view">
+        <img src="img/cloudy.svg" width="140px" alt="sunny">
+        <p class="degree js-fill">${temperature}°C</p>
+        <p class="city js-fill">${city}, ${country}</p>
+        <p class="position js-fill">${description}</p>
+    </div>
+    <div class="details">
+        <div class="wind box-details">
+            <p class="details__num js-fill">${windSpeed}</p>
+            <img src="img/windy.svg" width="24px" alt="windy">
+            <p class="details__text">Wind Flow</p>
+        </div>
+        <div class="feels_like box-details">
+            <p class="details__num js-fill">${feelslike}</p>
+            <img src="img/feels_like.svg" width="24px" alt="feels like">
+            <p class="details__text">Feels Like</p>
+        </div>
+        <div class="humidity box-details">
+            <p class="details__num js-fill">${humidity}</p>
+            <img src="img/humidity.svg" width="24px" alt="humidity">
+            <p class="details__text">Humidity</p>
+        </div>
+    </div>
+    `;
+};
+
+const renderComponent = () => {
+    node.innerHTML = markup();
+};
+
+const showData = () => {
+    node.classList.remove('dis-none');
+    searchForm.classList.add('dis-none');
+};  
+
+const handleInput = (e) => {
+    database = {
+        ...database,
+        city: e.target.value,
+    }
+};
+
+const handleSumbit = (e) => {
+    e.preventDefault();
+    console.log(database.city);
+    fetchData();
+};
+
+searchBtn.addEventListener('click', showData);
+searchInput.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSumbit);
+
+fetchData();
